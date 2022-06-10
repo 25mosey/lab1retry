b8352a28aba4558677b4451437f01e0e2adbfb15
никита матросов <nikm100804@gmail.com>
2022-06-10 19:33:36 +0200

    Добавьте файлы проекта.

---
+++ lab1retry/gipotenyza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1retry
+{
+    internal class gipotenyza
+    {
+        public double a;
+        public double b;
+        public double c;
+    }
+}
